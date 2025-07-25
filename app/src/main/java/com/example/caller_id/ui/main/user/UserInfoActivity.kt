954cb1b63901e0a2426c20420420ebe1ed532a92
Doan379a <doan379a@gmail.com>
2025-07-25 15:07:51 +0700

    update lan cuoi

--- app/src/main/java/com/example/caller_id/ui/main/user/UserInfoActivity.kt
+++ app/src/main/java/com/example/caller_id/ui/main/user/UserInfoActivity.kt
@@ -2,6 +2,8 @@ package com.example.caller_id.ui.main.user
 
 import com.example.caller_id.base.BaseActivity
 import com.example.caller_id.databinding.ActivityUserInfoBinding
+import com.example.caller_id.widget.showSnackBar
+import com.example.caller_id.widget.tap
 
 class UserInfoActivity:BaseActivity<ActivityUserInfoBinding>() {
     override fun setViewBinding(): ActivityUserInfoBinding {
@@ -9,6 +11,12 @@ class UserInfoActivity:BaseActivity<ActivityUserInfoBinding>() {
     }
 
     override fun initView() {
+        binding.tvSave.tap {
+            showSnackBar("Save")
+        }
+        binding.imgBack.tap {
+            finish()
+        }
     }
 
     override fun viewListener() {
